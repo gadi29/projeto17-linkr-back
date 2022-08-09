45fd475240fd984259d6fc263dd837e5d5de5df9
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-08-09 17:49:16 -0300

    create the middleware validateUser and create the insert sesion query

--- src/db/database.js
+++ src/db/database.js
@@ -5,11 +5,15 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-const connection = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL
+};
 
+if(process.env.MODE === "PROD") {
+  configDatabase.ssl = {
+    rejectUnauthorized: false
+  }
+}
+
+const connection = new Pool(configDatabase);
 export default connection;
