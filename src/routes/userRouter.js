e43cff2f71d67d6e16bb4300e79635d6ffb4711e
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-08-09 17:37:39 -0300

    create searchUsers route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,5 +4,6 @@ import { getUserPage } from "../controllers/userControllers.js";
 const userRouter = Router();
 
 userRouter.get("/user/:id", getUserPage);
+userRouter.get("/user", searchUsers)
 
 export default userRouter;
