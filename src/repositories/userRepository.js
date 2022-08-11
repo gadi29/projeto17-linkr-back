3a58040517424bcb63ea6da84262636dd2abe8f0
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-08-11 15:13:55 -0300

    fix: new postUser array

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,22 +1,26 @@
 import connection from "../db/database.js";
 
-function getUser(id) {
-  return connection.query(
-    `SELECT name, "userPhoto"
-    FROM users WHERE id = $1`,
-    [id]
-  );
-}
-
 function getUserPosts(userId) {
   return connection.query(
-    `SELECT "content", "postUrl"
-    FROM posts WHERE posts."userId" = $1`,
+    `SELECT 
+      "users"."id" AS "userId",
+      "users"."name" AS "userName",
+      "users"."userPhoto" AS "userPhoto",
+      "posts".id AS "postId",
+      "posts"."postText",
+      "posts"."postUrl",
+      "posts"."urlTitle",
+      "posts"."urlDescription",
+      "posts"."urlImage"
+    FROM "posts"
+    JOIN "users" ON "posts"."userId" = "users"."id"
+    WHERE "posts"."userId" = $1
+    ORDER BY "posts"."createdAt" DESC
+    LIMIT 20`,
     [userId]
   );
 }
 
 export const userRepository = {
-  getUser,
   getUserPosts
 }
