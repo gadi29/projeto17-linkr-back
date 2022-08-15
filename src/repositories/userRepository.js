62042179df027ffdcd2155aa52d67360b1f514a7
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-08-15 14:46:46 -0300

    feat: change quey for get user posts with likes

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -19,9 +19,14 @@ function getUserPosts(userId) {
       "posts"."urlTitle",
       "posts"."urlDescription",
       "posts"."urlImage"
+      COUNT("likes"."postId")::int AS "likesQty",
+      COALESCE(JSON_AGG("likes"."userId") FILTER (WHERE "likes"."userId" IS NOT NULL), '[]'::json) AS "usersIdLiked",
+      COALESCE(JSON_AGG("likes"."userName") FILTER (WHERE "likes"."userName" IS NOT NULL), '[]'::json) AS "usersNameLiked"
     FROM "posts"
+    LEFT JOIN "likes" ON "posts"."id" = "likes"."postId"
     JOIN "users" ON "posts"."userId" = "users"."id"
     WHERE "posts"."userId" = $1
+    GROUP BY "posts"."id", "users"."name", "users"."id", "users"."userPhoto"
     ORDER BY "posts"."createdAt" DESC
     LIMIT 20`,
     [userId]
