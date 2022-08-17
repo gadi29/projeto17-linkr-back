c53ded650e5abfa75e4bd6bf6a80d63553ba77c9
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-08-17 13:53:10 -0300

    feat: create follow and unfollow routes

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -6,6 +6,8 @@ const userRouter = Router();
 
 userRouter.get("/user/:id", getUserPage);
 userRouter.get("/user/following/:id", validateUser, isFollowingUser);
+userRouter.post("/user/follow/:id", validateUser, followUser);
+userRouter.delete("user/unfollow/:id", validateUser, unfollowUser);
 userRouter.get("/user", searchUsers);
 
 export default userRouter;
